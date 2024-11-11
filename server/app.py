c5a048639ec1d447fc1da6b6944e03cc18f1c6d0
Amos Oluoch <aoluoch271@gmail.com>
2024-11-11 14:30:46 +0300

    test2

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return "Hello, World!, Welcome to your work flow!"
 
 if __name__ == "__main__":
     app.run(debug=True)
