ab3ce13b200f6ae70f588755f849e44db1afa3ff
Amos Oluoch <aoluoch271@gmail.com>
2024-11-12 15:19:25 +0300

    user test

--- server/tests/test_user.py
+++ server/tests/test_user.py
@@ -1,19 +1,15 @@
+import sys
+from unittest.mock import Mock
+
+# Mock sqlalchemy_serializer if it doesn't exist
+sys.modules['sqlalchemy_serializer'] = Mock()
+
 from models.user import User
 
 def test_table_name():
-    """
-    GIVEN a User model
-    WHEN checking the table name
-    THEN ensure it is 'users'
-    """
     assert User.__tablename__ == 'users'
 
 def test_user_fields():
-    """
-    GIVEN a User model
-    WHEN a new User is created
-    THEN check the presence of the fields
-    """
     user = User()
     assert hasattr(user, 'id')
     assert hasattr(user, 'username')
