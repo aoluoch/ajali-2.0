5a91e6a9a077e5571a5aa7008ae3349b9deb9a09
Amos Oluoch <aoluoch271@gmail.com>
2024-11-15 18:15:26 +0300

    user test

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -5,24 +5,3 @@ def test_index():
     assert response.status_code == 200
     assert response.data == b"Hello, World!, Welcome to your work flow!"
 
-from models.user import User
-
-def test_user_fields():
-    """
-    GIVEN a User model
-    WHEN a new User is created
-    THEN check the presence of the required fields
-    """
-    user = User()
-
-    # Check the presence of fields in the User model (excluding relationships)
-    assert hasattr(user, 'id')
-    assert hasattr(user, 'username')
-    assert hasattr(user, 'email')
-    assert hasattr(user, 'password_hash')
-    assert hasattr(user, 'is_admin')
-
-    # Exclude checking the 'reports' relationship
-    assert not hasattr(user, 'reports')
-
-
